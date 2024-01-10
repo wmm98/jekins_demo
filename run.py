64570efc5da780d1b5357aa54fd3f36a7a5c5bdc
tom-gitee <792545884@qq.com>
2024-01-10 21:49:45 +0800

    update

--- run.py
+++ run.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
     pytest.main()
     current_directory = os.getcwd()
     # print(path_dir)
-    xml_path = current_directory + '\\allure-report\\xml'
-    html_path = current_directory + '\\allure-report\\html'
-    report_path = current_directory + "\\allure-report"
+    xml_path = current_directory + '\\Report\\xml'
+    html_path = current_directory + '\\Report\\html'
+    report_path = current_directory + "\\Report"
 
     cmd = "allure generate %s -o %s --clean" % (xml_path, html_path)
     output, errors = subprocess.Popen(str(cmd), shell=True, stdout=subprocess.PIPE,
